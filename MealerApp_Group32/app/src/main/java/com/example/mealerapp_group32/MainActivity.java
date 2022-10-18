122b9c84d4ad419caa389ee0f4f570aea2c43d69
Armand Guigma <aguig043@uottawa.ca>
2022-10-18 16:28:41 -0400

    firbase added

--- MealerApp_Group32/app/src/main/java/com/example/mealerapp_group32/MainActivity.java
+++ MealerApp_Group32/app/src/main/java/com/example/mealerapp_group32/MainActivity.java
@@ -3,11 +3,14 @@ package com.example.mealerapp_group32;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.firebase.auth.FirebaseAuth;
 
+public class MainActivity extends AppCompatActivity {
+    private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mAuth = FirebaseAuth.getInstance();
     }
 }
